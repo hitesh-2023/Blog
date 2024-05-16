0d1d12522b306bfc3559b045e772d2b2f3706498
hitesh-realbotz <hitesh.patil@realbotz.com>
2024-05-16 18:44:31 +0530

    Simple Blog - Navbar + manual contentful api response handling

--- simple-blog-demo/src/app/articles/[slug]/page.tsx
+++ simple-blog-demo/src/app/articles/[slug]/page.tsx
@@ -1,12 +1,11 @@
-import { BlogItem, ExtendedBlogItem } from "@/app/types";
+import { BlogItem, BlogPageProps } from "@/app/types";
 import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 import { createClient } from "contentful";
-import { BlogPageProps } from "@/app/types";
+import { getBlogItem } from "@/utils/type-cast.utils";
 
 if (!process.env.SPACE_ID || !process.env.ACCESS_TOKEN) {
     throw new Error('SPACE_ID or ACCESS_TOKEN is not provided');
 }
-
 const client = createClient({
     space: process.env.SPACE_ID,
     accessToken: process.env.ACCESS_TOKEN,
@@ -29,19 +28,22 @@ const fetchBlogPost = async (slug: string): Promise<BlogItem> => {
         content_type: "blog",
         "fields.slug[match]": slug,
     };
-    try {
-        const queryResult = await client.getEntries<ExtendedBlogItem>(queryOptions);
-        return queryResult.items[0];
-    } catch (error) {
-        throw new Error(`Error fetching blog post: ${error}`);
+    const queryResult = await client.getEntries(queryOptions);
+    // Ensure that at least one item is returned
+    if (queryResult.items.length === 0) {
+        throw new Error(`No blog post found with slug '${slug}'.`);
     }
+
+    // Return BlogItem
+    return getBlogItem(queryResult.items[0].fields);
 };
 
 export default async function BlogPage(props: BlogPageProps) {
     const { params } = props;
     const { slug } = params;
     const article = await fetchBlogPost(slug);
-    const { title, date, content } = article.fields;
+    const { title, date, content, thumbnail } = article.fields;
+    const imageUrl = thumbnail.fields.file.url;
 
     return (
         <main className="min-h-screen p-24 flex justify-center">
@@ -56,6 +58,12 @@ export default async function BlogPage(props: BlogPageProps) {
                         day: "numeric",
                     })}
                 </p>
+                <img
+                    src={imageUrl}
+                    alt={title}
+                    style={{ width: "100%", height: "30%", maxHeight: "30vh" }}
+                    className="rounded-lg w-full object-cover md:h-full md:w-48"
+                />
                 <div className="[&>p]:mb-8 [&>h2]:font-extrabold">
 
                     {documentToReactComponents(content)}
@@ -64,113 +72,3 @@ export default async function BlogPage(props: BlogPageProps) {
         </main>
     );
 }
-
-////Manual Handling Contentful API response
-// import { BlogFields } from "@/app/types";
-// import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
-// import { createClient } from "contentful";
-// import { BlogPageProps } from "@/app/types";
-// import { Document, BLOCKS, TopLevelBlock } from '@contentful/rich-text-types';
-
-// if (!process.env.SPACE_ID || !process.env.ACCESS_TOKEN) {
-//     throw new Error('SPACE_ID or ACCESS_TOKEN is not provided');
-// }
-// const client = createClient({
-//     space: process.env.SPACE_ID,
-//     accessToken: process.env.ACCESS_TOKEN,
-// });
-
-// export async function generateStaticParams() {
-//     const queryOptions = {
-//         content_type: "blog",
-//         select: "fields.slug",
-//     };
-
-//     const articles = await client.getEntries(queryOptions);
-//     return articles.items.map((article) => ({
-//         slug: article.fields.slug,
-//     }));
-// }
-
-// const fetchBlogPost = async (slug: string): Promise<BlogFields> => {
-//     const queryOptions = {
-//         content_type: "blog",
-//         "fields.slug[match]": slug,
-//     };
-//     const queryResult = await client.getEntries(queryOptions);
-//     // Ensure that at least one item is returned
-//     if (queryResult.items.length === 0) {
-//         throw new Error(`No blog post found with slug '${slug}'.`);
-//     }
-
-//     const blogFieldsString = JSON.stringify(queryResult.items[0].fields);
-//     const parsedblogFields = JSON.parse(blogFieldsString);
-//     const { date, title, content } = parsedblogFields;
-
-//     // Return BlogFields
-//     return {
-//         title: title,
-//         slug: slug,
-//         date: new Date(date),
-//         mainNodeType: content.nodeType,
-//         mainData: content.data,
-//         contents: content.content
-//     };
-// };
-
-// export default async function BlogPage(props: BlogPageProps) {
-//     const { params } = props;
-//     const { slug } = params;
-//     const article = await fetchBlogPost(slug);
-//     const { title, date, mainNodeType, mainData, contents } = article;
-
-//     const contentNodes: TopLevelBlock[] = [];
-
-//     contents.forEach((item: any) => {
-//         switch (item.nodeType) {
-//             case 'paragraph':
-//                 contentNodes.push({
-//                     nodeType: BLOCKS.PARAGRAPH,
-//                     content: item.content,
-//                     data: item.data
-//                 });
-//                 break;
-//             case 'heading-3':
-//                 contentNodes.push({
-//                     nodeType: BLOCKS.HEADING_3,
-//                     content: item.content,
-//                     data: item.data
-//                 });
-//                 break;
-//             // Add cases for other nodeTypes as needed
-//         }
-//     });
-
-//     // Create Document object
-//     const document: Document = {
-//         nodeType: BLOCKS.DOCUMENT,
-//         data: mainData,
-//         content: contentNodes
-//     };
-
-//     return (
-//         <main className="min-h-screen p-24 flex justify-center">
-//             <div className="max-w-2xl">
-//                 <h1 className="font-extrabold text-3xl mb-2">{title}</h1>
-
-//                 <p className="mb-6 text-slate-400 ">
-//                     Posted on{" "}
-//                     {new Date(date).toLocaleDateString("en-US", {
-//                         year: "numeric",
-//                         month: "long",
-//                         day: "numeric",
-//                     })}
-//                 </p>
-//                 <div className="[&>p]:mb-8 [&>h2]:font-extrabold">
-
-//                     {documentToReactComponents(document)}
-//                 </div>
-//             </div>
-//         </main>
-//     );
-// }
